cbdb999d12b056d8d68e2161bcdbc9b7b47b798d
kabue442 <josephwangare253@gmail.com>
2021-03-16 12:53:25 +0300

    addd index.css index.html script.js

---
+++ script.js
@@ -0,0 +1,141 @@
+var shedA = shedA(510, 45);
+document.getElementById("shed") .innerHTML= "shed A" + shedA;
+function shedA (litres, shillings){
+    return litres*shillings;
+
+}
+console.log(shedA)
+
+var shedB=shedB(308, 45);
+document.getElementById("two") .innerHTML="shed B" + shedB;
+function shedB(litres, shillings){
+    return litres*shillings;
+}
+console.log(shedB)
+
+var shedC=shedC(486, 45);
+document.getElementById("three") .innerHTML="shed C" + shedC;
+function shedC(litres, shillings){
+    return litres*shillings;
+}
+console.log(shedC)
+
+var shedD=shedD(572, 45);
+document.getElementById("four") .innerHTML="shed D" + shedD;
+function shedD(litres, shillings){
+    return litres*shillings;
+}
+console.log(shedD)
+
+var Total=Total(1872, 45);
+document.getElementById("five") .innerHTML="Total" + Total;
+function Total(litres, shillings){
+    return litres*shillings
+}
+console.log(Total)
+
+
+
+var weekly=weekly(590940, 7);
+document.getElementById("once") .innerHTML="weekly" + weekly;
+function weekly(shillings, days){
+    return shillings*days
+}
+console.log(weekly)
+
+var yearly=yearly(30813300, 365);
+document.getElementById("twice") .innerHTML="yearly" + yearly;
+function yearly(shillings, days){
+    return shillings*days
+}
+console.log(yearly)
+
+var january=january(26170020,31);
+document.getElementById("jan").innerHTML="january"+ january;
+function january(shillings, days){
+    return shillings*days
+}
+console.log(january)
+
+var february=february(2448180, 29);
+document.getElementById("feb") .innerHTML="february" + february;
+function february(shillings, days){
+    return shillings*days
+}
+console.log(february)
+
+
+var march=march(26170020, 31);
+document.getElementById("mar") .innerHTML="march" + march;
+function march(shillings, days){
+    return shillings*days
+}
+console.log(march)
+
+
+var April=April(2532600, 30);
+document.getElementById("apr") .innerHTML="april" + April;
+function April(shillings, days){
+    return shillings*days
+}
+console.log(April)
+
+var may=may(261720, 31);
+document.getElementById("may") .innerHTML="may" + may;
+function may(shillings, days){
+    return shillings*days
+}
+console.log(may)
+
+
+var june=june(2523600, 30);
+document.getElementById("jun") .innerHTML="june"+ june;
+function june(shillings, days){
+    return shillings*days
+}
+console.log(june)
+
+
+
+var july=july(26170020,31);
+document.getElementById("jul") .innerHTML="july" + july;
+function july(shillings,days){
+    return shillings*days
+}
+console.log(july)
+
+
+var August=August(2532600, 31)
+document.getElementById("aug") .innerHTML="August" + August;
+function August(shillings, days){
+    return shillings*days
+}
+console.log(August)
+
+var september=(30, 2532600);
+document.getElementById("sept") .innerHTML="september" + september;
+function september(shillings, days){
+    return shillings*days
+}
+console.log(september)
+
+var october=october(2617020,31);
+document.getElementById("oct") .innerHTML="october" + october;
+function october(shillings, days){
+    return shillings*days
+}
+console.log(october)
+
+var november=november(2532600,30);
+document.getElementById("nov") .innerHTML="november" + november;
+function november(shillings, days){
+    return shillings*days
+}
+console.log(november)
+
+var december=december(2617020, 31);
+document.getElementById("dec") .innerHTML="december" + december;
+function december(shillings, days){
+    return shillings*days
+}
+console.log(december)
